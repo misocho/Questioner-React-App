55bd0917467061d4231a4b7373ecad11053e2992
misocho <misochobrian@gmail.com>
2019-05-15 14:18:52 +0300

    feature(homepage): Add the homepage
    - Adds the homepage
    
    [Delivers]

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,23 +1,22 @@
 import React from "react";
 
-import Navigation from '../components/Navigation';
-import LoginForm from '../components/Login';
+import Navigation from "../components/Navigation";
+import LoginForm from "../components/Login";
 
-import '../css/input.css';
+import "../css/input.css";
 
 class Login extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navigation></Navigation>
-                <div className="page-body">
-                <div className="container">
-                <LoginForm></LoginForm>
-            </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="page-body">
+          <div className="container">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
